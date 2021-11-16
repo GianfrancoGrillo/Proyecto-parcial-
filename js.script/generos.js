2dace2b3895ff29f07ffba65db06b72a613ee104
GianfrancoGrillo <89219968+GianfrancoGrillo@users.noreply.github.com>
2021-11-16 15:30:54 -0300

    generos

--- js.script/generos.js
+++ js.script/generos.js
@@ -8,22 +8,21 @@ fetch(url)
     })
 
     .then(function (data) {
-        let array = data.results;
+       let info = data.genres;
         let genero = document.querySelector('#generoPelicula')
         let genPelicula = ""
-        for (let i = 0; i < 20; i++) {
+       
+        for (let i = 0; i < info.length; i++) {
             genPelicula +=
             `
-            <a href="detalleGenero?id=${array[i].id}">
-                <h3>${array[i].name}</h3>
+            <a href="detalleGenero?id=${info[i].id}">
+                <h3>${info[i].name}</h3>
              </a>
         `;
-
-                genero.innerHTML=genPelicula
-
+  
+        
         }
-
-
+        genero.innerHTML=genPelicula 
 
         console.log(data);
     })
@@ -31,3 +30,41 @@ fetch(url)
         console.log("Error: " + error);
     })
 
+
+
+// FETCH DE GENERO DE SERIES
+
+let urll= 'https://api.themoviedb.org/3/genre/tv/list?api_key=c96a79dd3a41e33fa75fda92e7e71ee6'  
+
+    fetch(urll)
+    .then(function(response) {
+      return response.json()
+    })
+
+
+    .then(function(data) {
+        let info = data.genres;
+        let genero = document.querySelector('#generoSeries')
+        let genPelicula = ""
+       
+        for (let i = 0; i < info.length; i++) {
+            genPelicula +=
+            `
+            <a href="detalleGenero?id=${info[i].id}">
+                <h3>${info[i].name}</h3>
+             </a>
+        `;
+  
+        
+        }
+        genero.innerHTML=genPelicula 
+
+
+
+
+      console.log(data);
+    })
+    .catch(function(error) {
+      console.log("Error: " + error);
+    })
+    
